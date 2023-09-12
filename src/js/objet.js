b4f2e22bb7681ce07602193237dbbb9be0750691
AdrienGendron <112941884+AdrienGend@users.noreply.github.com>
2023-09-12 15:08:27 +0200

    add img a objet.js

--- src/js/objet.js
+++ src/js/objet.js
@@ -17,41 +17,41 @@ const item1 = new Item("Amulette de richesse", "Augmente l'or de base du joueur"
 
 // Objet 2 : augmente le niveau de foi de base du joueur.
 const item2 = new Item("Relique sacrée", "Augmente le niveau de foi de base du joueur", 0, () => {
-    player.changeFaith(10);"./image/reliquesacree.png";
+    player.changeFaith(10);"./image/artefactsacree.png";
 });
 
 // Objet 3 : augmente légèrement l'or et le niveau de foi de base du joueur.
 const item3 = new Item("Talisman béni", "Augmente légèrement l'or et le niveau de foi de base du joueur", 0, () => {
     player.changeGold(5);
-    player.changeFaith(5);
+    player.changeFaith(5);"./image/talismanbeni.png"
 });
 
 // Objet 4 : Talisman de Prospérité
 const item4 = new Item("Talisman de Prospérité", "Évite la banqueroute en maintenant l'or du joueur à 1 lorsque le joueur atteint 0 d'or.", 0, () => {
     if (player.gold === 0) {
-        player.changeGold(1);
-    }
+        player.changeGold(1)};"./image/talismandeprosperité.png"
+    });
 
 // Objet 5 : L'Épée de la Vertu
 const item5 = new Item("L'Épée de la Vertu", "Ajoute +10 de foi.", 0, () => {
-    player.changeFaith(10);
+    player.changeFaith(10);"./image/epeevertu.png"
 });
 
 
 // Objet 6 : Amulette de l'Évasion
 const item6 = new Item("Amulette de l'Évasion", "Permet au joueur de passer un évènement en cours de jeu, évitant ainsi ses conséquences. L'amulette est détruite après utilisation.", 0, () => {
     // TODO
-});
+});"./image/amuletteevasion.png"
 
 // Objet 7 : Corne de l'Abondance
 const item7 = new Item("Corne de l'Abondance", "Permet au joueur d'augmenter son stock d'or une fois par période de jeu.", 1, () => {
     player.changeGold(10);
-});
+});"./image/corned_abondance.png"
 
 // Objet 8 : Bouclier Divin
 const item8 = new Item("Bouclier Divin", "Augmente le pouvoir militaire de 10.", 2, () => {
     player.changeArmy(10);
-});
+});"./image/bouclier.png"
 
 
 console.log(item1);
