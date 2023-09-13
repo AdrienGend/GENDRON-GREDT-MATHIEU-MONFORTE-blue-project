b60a9f317458d09c8abaaea2ff0f491aa4fe99a1
MathisGredtYnov <mathis.gredt@ynov.com>
2023-09-13 23:06:48 +0200

    optimisation de zinzin sur l'affichage des events

--- src/js/game.js
+++ src/js/game.js
@@ -102,12 +102,10 @@ function displayInventory() {
     }
 }
 
-
-
-
 displayBackground();
-//function affichage Mage
-function displayMageAvatar() {
+
+
+function displayAvatar(imagePath, cssClass, dialogText, buttonLeftText, buttonRightText) {
     let center = document.querySelector(".center");
 
     // Créer une div pour le personnage et la bulle de dialogue
@@ -116,13 +114,13 @@ function displayMageAvatar() {
 
     // Créer l'image du personnage
     let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Mage.png";
-    characterImg.classList.add("gauche");
+    characterImg.src = imagePath;
+    characterImg.classList.add(cssClass);
 
     // Créer la bulle de dialogue
     let dialogBox = document.createElement("div");
     dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
+    dialogBox.textContent = dialogText;
 
     // Ajouter l'image du personnage et la bulle de dialogue au conteneur
     characterContainer.appendChild(characterImg);
@@ -134,320 +132,128 @@ function displayMageAvatar() {
     // Ajouter un bouton dans la div "Left"
     let leftDiv = document.querySelector(".Left");
     let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
+    leftButton.textContent = buttonLeftText;
     leftDiv.appendChild(leftButton);
 
     // Ajouter un bouton dans la div "Right"
     let rightDiv = document.querySelector(".Right");
     let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
+    rightButton.textContent = buttonRightText;
     rightDiv.appendChild(rightButton);
 }
 
+// Appeler la fonction pour afficher le Mage
+function displayMageAvatar() {
+    displayAvatar(
+        "image/Personnage/Mage.png",
+        "gauche",
+        "Ceci est le texte de la bulle de dialogue du Mage.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
+}
 
-//function affichage Archer
+// Appeler la fonction pour afficher l'Archer
 function displayArcherAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Archer.png";
-    characterImg.classList.add("droite");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Archer.png",
+        "droite",
+        "Ceci est le texte de la bulle de dialogue de l'Archer.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 //function affichage Paysan 
 function displayPeasantAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Paysan.png";
-    characterImg.classList.add("droite");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Paysan.png",
+        "gauche",
+        "Ceci est le texte de la bulle de dialogue du Paysan.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
+    
 }
 
 //function affichage refugier
 function displayRefugeeAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Refugier.png";
-    characterImg.classList.add("droite");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Refugier.png",
+        "droite",
+        "Ceci est le texte de la bulle de dialogue du Refugier.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 //function affichage Pape  
 function displayPapeAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Pape.png";
-    characterImg.classList.add("gauche");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Pape.png",
+        "gauche",
+        "Ceci est le texte de la bulle de dialogue du Pape.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 
 //function affichage Bouffon
 function displayJesterAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Bouffon.png";
-    characterImg.classList.add("gauche");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Bouffon.png",
+        "droite",
+        "Ceci est le texte de la bulle de dialogue du Bouffon.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 //function affichage Capitaine des soldats 
 function displayCaptainAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Capitaine.png";
-    characterImg.classList.add("gauche");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Capitaine.png",
+        "gauche",
+        "Ceci est le texte de la bulle de dialogue du Capitaine.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 //function affichage Chevalier 
 function displayKnightAvatar() {
-    let center = document.querySelector(".center");
-
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Chevalier.png";
-    characterImg.classList.add("gauche");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+    displayAvatar(
+        "image/Personnage/Chevalier.png",
+        "droite",
+        "Ceci est le texte de la bulle de dialogue du Chevalier.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 //function affichage conseiller 
 function displayAdvisorAvatar() {
-    let center = document.querySelector(".center");
+    displayAvatar(
+        "image/Personnage/Conseiller.png",
+        "gauche",
+        "Ceci est le texte de la bulle de dialogue du Conseiller.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
+}
 
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
 
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Conseiller.png";
-    characterImg.classList.add("droite");
 
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("button");
-    leftButton.textContent = "Bouton Gauche";
-    leftDiv.appendChild(leftButton);
-
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("button");
-    rightButton.textContent = "Bouton Droite";
-    rightDiv.appendChild(rightButton);
+//function affichage Marchant
+function displayMarchandAvatar() {
+    displayAvatar(
+        "image/Personnage/Marchand.png",
+        "droite",
+        "Ceci est le texte de la bulle de dialogue du Marchand.",
+        "Bouton Gauche",
+        "Bouton Droite"
+    );
 }
 
 function Getfromjson() {
@@ -469,46 +275,7 @@ function Getfromjson() {
 
 }
 
-//function affichage Marchant
-function displayMarchandAvatar() {
-    let center = document.querySelector(".center");
 
-    // Créer une div pour le personnage et la bulle de dialogue
-    let characterContainer = document.createElement("div");
-    characterContainer.classList.add("character-container");
-
-    // Créer l'image du personnage
-    let characterImg = document.createElement("img");
-    characterImg.src = "image/Personnage/Marchand.png";
-    characterImg.classList.add("gauche");
-
-    // Créer la bulle de dialogue
-    let dialogBox = document.createElement("div");
-    dialogBox.classList.add("dialog-box");
-    dialogBox.textContent = "Ceci est le texte de la bulle de dialogue.";
-
-    // Ajouter l'image du personnage et la bulle de dialogue au conteneur
-    characterContainer.appendChild(characterImg);
-    characterContainer.appendChild(dialogBox);
-
-    // Ajouter le conteneur du personnage et de la bulle de dialogue au centre
-    center.appendChild(characterContainer);
-
-    // Ajouter un bouton dans la div "Left"
-    let leftDiv = document.querySelector(".Left");
-    let leftButton = document.createElement("div");
-    leftButton.classList.add("SpecialMarchandAcheter")
-    leftButton.innerHTML = "<div class=" + "TitleShop" + ">Objets Achetable</div>";
-    leftDiv.appendChild(leftButton);
-    
-    // Ajouter un bouton dans la div "Right"
-    let rightDiv = document.querySelector(".Right");
-    let rightButton = document.createElement("div");
-    rightButton.classList.add("SpecialMarchandVendre")
-    rightButton.innerHTML = "<div class=" + "TitleShop" + ">Objets Vendable</div>"
-    "<div class=" + "ItemShop" + ">Objets Vendable</div>";
-    rightDiv.appendChild(rightButton);
-}
 //function affichage perso 
 function displayPersoAvatar(event) {
     console.log("displayPersoAvatar");
